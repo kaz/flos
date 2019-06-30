afc1bbe1618bb222d531b487d3abb2478c608ddf
Kazuki Sawada <kazuki@6715.jp>
2019-06-30 20:49:47 +0900

    re-impl: limit setting

--- limit/limit.go
+++ limit/limit.go
@@ -1,9 +1,11 @@
 package limit
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 var (
@@ -11,14 +13,63 @@ var (
 )
 
 func Init() {
-	run("ulimit", "-n", "1000000000")
-	run("sysctl", "-w", "fs.inotify.max_user_watches=2147483647")
+	if err := setNoFileLimit(); err != nil {
+		logger.Printf("failed to set RLIMIT_NOFILE: %v\n", err)
+	}
+	if err := setInotifyLimit(); err != nil {
+		logger.Printf("failed to set fs.inotify.max_user_watches: %v\n", err)
+	}
 }
 
-func run(name string, arg ...string) {
-	out, err := exec.Command(name, arg...).CombinedOutput()
-	if err != nil {
-		logger.Println(err)
+func setNoFileLimit() error {
+	var limit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+		return fmt.Errorf("failed to get rlimit: %v\n", err)
 	}
-	logger.Println(string(out))
+
+	min := limit.Cur
+	max := limit.Max
+
+	for max-min > 1 {
+		mid := (min + max) / 2
+		limit.Cur = mid
+
+		syscall.Setrlimit(syscall.RLIMIT_NOFILE, &limit)
+		if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+			return fmt.Errorf("failed to get rlimit: %v\n", err)
+		}
+
+		if limit.Cur == mid {
+			min = mid
+		} else {
+			max = mid
+		}
+	}
+
+	logger.Printf("RLIMIT_NOFILE set to %v\n", limit)
+	return nil
+}
+
+func setInotifyLimit() error {
+	out, err := exec.Command("bash", "-c", `
+		min=0
+		max=$(( 1 << 62 ))
+
+		while (( max - min > 1 )); do
+			mid=$(( (min + max) >> 1 ))
+			if echo $mid > /proc/sys/fs/inotify/max_user_watches; then
+				min=$mid
+			else
+				max=$mid
+			fi
+		done
+
+		echo $mid | tee /proc/sys/fs/inotify/max_user_watches
+	`).Output()
+	if err != nil {
+		return err
+	}
+
+	logger.Printf("fs.inotify.max_user_watches set to %s\n", out)
+	return nil
 }
