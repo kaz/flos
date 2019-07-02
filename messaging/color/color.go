d5ba6d1c2f6a37c473d92e365c8f4120cb8bcc71
Kazuki Sawada <kazuki@6715.jp>
2019-07-03 02:35:36 +0900

    color proto: lightweight signing

--- messaging/color/color.go
+++ messaging/color/color.go
@@ -3,6 +3,7 @@ package color
 import (
 	"crypto/hmac"
 	"crypto/md5"
+	"encoding/binary"
 	"fmt"
 	"time"
 
@@ -14,21 +15,21 @@ const (
 	// signature valid in 15s
 	VALID_THRU = 15 * time.Second
 
-	SIGN_KEY = "Daphne Ficus Iris Maackia Lythrum Myrica Sabia Flos"
+	SIGN_KEY = "Lythrum Myrica Sabia Flos"
 )
 
 type (
 	Protocol struct{}
-
-	StampedPayload struct {
-		Payload   []byte
-		Timestamp int64
-	}
-	SignedPayload struct {
-		Payload   []byte
-		Signature []byte
-	}
 )
+
+func itob(i int64) []byte {
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, uint64(i))
+	return key
+}
+func btoi(b []byte) int64 {
+	return int64(binary.LittleEndian.Uint64(b))
+}
 
 func serialize(obj interface{}) ([]byte, error) {
 	return msgpack.Encode(obj)
@@ -38,48 +39,46 @@ func deserialize(data []byte, objPtr interface{}) error {
 }
 
 func sign(data []byte) ([]byte, error) {
-	stamped, err := serialize(&StampedPayload{
-		data,
-		time.Now().UnixNano(),
-	})
-	if err != nil {
-		return nil, err
-	}
+	data = append(data, itob(time.Now().UnixNano())...)
 
 	m := hmac.New(md5.New, []byte(SIGN_KEY))
-	if _, err := m.Write(stamped); err != nil {
+	if _, err := m.Write(data); err != nil {
 		return nil, err
 	}
 
-	return serialize(&SignedPayload{
-		stamped,
-		m.Sum(nil),
-	})
+	return append(data, m.Sum(nil)...), nil
 }
 func verify(data []byte) ([]byte, error) {
-	signed := &SignedPayload{}
-	if err := deserialize(data, signed); err != nil {
-		return nil, err
+	p := len(data) - md5.Size
+	if p <= 0 {
+		return nil, fmt.Errorf("invalid size")
 	}
+
+	signature := data[p:]
+	data = data[:p]
 
 	m := hmac.New(md5.New, []byte(SIGN_KEY))
-	if _, err := m.Write(signed.Payload); err != nil {
+	if _, err := m.Write(data); err != nil {
 		return nil, err
 	}
 
-	if !hmac.Equal(signed.Signature, m.Sum(nil)) {
+	if !hmac.Equal(m.Sum(nil), signature) {
 		return nil, fmt.Errorf("signature not match")
 	}
 
-	stamped := &StampedPayload{}
-	if err := deserialize(signed.Payload, stamped); err != nil {
-		return nil, err
+	p = len(data) - 8
+	if p <= 0 {
+		return nil, fmt.Errorf("invalid size")
 	}
-	if time.Since(time.Unix(0, stamped.Timestamp)) > VALID_THRU {
+
+	ts := data[p:]
+	data = data[:p]
+
+	if time.Since(time.Unix(0, btoi(ts))) > VALID_THRU {
 		return nil, fmt.Errorf("signature expired")
 	}
 
-	return stamped.Payload, nil
+	return data, nil
 }
 
 func (p *Protocol) Encode(obj interface{}) ([]byte, error) {
